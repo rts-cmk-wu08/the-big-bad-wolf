93f455ca376e1e06483e057bb1a45b5dff2947fd
In-Wess-Tation <skolekonto318@gmail.com>
2022-12-06 11:06:47 +0100

    about js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,5 @@
+import "./about.css"
+
 const About = () => {
 
     return (
@@ -11,7 +13,7 @@ const About = () => {
 
                 <p>Our Edinburgh branch has the longest history as an audio retailer in the UK. During recent renovations, receipts were found from Nicolson’s Gramophone Saloon dating back to 1926. In the 1950s WG Graham took over the shop and renamed it WG Graham’s Hi-Fi Corner. Upon his retirement, Graham Tiso bought the business and shortened the name to Hi-Fi Corner.  
 
-                    Soon thereafter a young enthusiastic Colin MacKenzie (left), who was recommended by Linn’s own Ivor Tiefenbrun, was employed to manage the shop; with a knack for business and years of experience in the hi-fi industry, Colin would later become the owner of Hi-Fi Corner. Today, Struan  MacKenzie carries on the legacy as the company’s Managing Director. 
+                    Soon thereafter a young enthusiastic Colin MacKenzie (left), who was recommended by Linn’s own Ivor Tiefenbrun, was employed to manage the shop; with a knack for business and years of experience in the hi-fi industry, Colin would later become the owner of Hi-Fi Corner. Today, Struan MacKenzie carries on the legacy as the company’s Managing Director. 
                 </p>
             </section>
             <section class="item__img"><img src="1.jpg" height="275" width="450"/></section>
