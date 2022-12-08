f7896c0be51946eb56735be673ccdf9fdf0bccbd
Max Hargreave <max@monsun.dk>
2022-12-08 13:49:34 +0100

    error fix

--- src/components/ShopCard.js
+++ src/components/ShopCard.js
@@ -6,11 +6,21 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/opacity.css';
 import "./card.scss";
 
-const Card = (props) => {
+const ShopCard = (props) => {
 
     const product = props.attributes
     const link = '/shop/' + props.id
 
+    let imagesArray;
+
+    try {
+        // Try to get the values of the item's values property
+        imagesArray = Object.values(product.Images);
+    } catch (error) {
+        // Set values to an empty array if there is an error
+        imagesArray = [];
+    }
+    
     const [compCards, setCompCards] = useContext(CompareContext);
 
         return (
@@ -35,20 +45,20 @@ const Card = (props) => {
                 
                 <div className="card__img" >
 
-                    {Object.values(product?.Images).map(img => ( 
+                    {imagesArray.map(img => ( 
                         <Link to={link} key={img[0].id} ><LazyLoadImage effect="opacity" src={img[0].attributes.url} alt={product.Name} /></Link>
                     ))}
 
 
                 </div>
 
-                <h3 className="card__title"><Link to={link}>{product.Name}</Link></h3>
-                <p className='card__price'>£ {product.Price}.00</p>
+                <h3 className="card__title"><Link to={link}>{product?.Name}</Link></h3>
+                <p className='card__price'>£ {product?.Price}.00</p>
                 <div className='card__submit'>
                     
                     <button className="btn card__btn" >Add to cart</button>
                 
-                    <span className="addtocart__stock">{product.Stock} <span className="inStock"></span></span>
+                    <span className="addtocart__stock">{product?.Stock} <span className="inStock"></span></span>
                 </div>
 
             </div>
@@ -58,4 +68,4 @@ const Card = (props) => {
 
 };
 
-export default Card;
+export default ShopCard;
