ee25ea50c63e898f3a0a0a4ea37941c250178fc8
Max Hargreave <max@monsun.dk>
2022-12-10 22:48:02 +0100

    functional cart widget

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,71 @@
+import { useContext, useState, useRef, useEffect } from "react";
+import { CartContext } from "../../contexts/CartContext";
+import { Link } from 'react-router-dom';
+import { IoCloseCircleOutline} from 'react-icons/io5';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import './CartWidget.scss';
+
+const CartWidget = () => {
+
+    const [cartItems, setCartItems] = useContext(CartContext);
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = event => { if (dropdownRef.current && !dropdownRef.current.contains(event.target)) { setIsOpen(false); } };
+        document.addEventListener("click", handleClickOutside);
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, [dropdownRef]);
+
+    const removeCard = (id) => {
+        setCartItems(cartItems.filter(cartItem => cartItem.id !== id ));
+    };
+
+    return ( 
+
+        <>
+            <span className="cart-widget">
+                <Link to={"/cart"} className="header-icons__link"
+                    onMouseEnter={() => setIsOpen(true)}
+                >
+                    <i className="fa-solid fa-cart-shopping cart-widget__icon"></i><span className="cart-widget__counter">{cartItems.length}</span>
+                </Link>
+                
+            </span>
+
+            {isOpen && (
+                <div ref={dropdownRef} className="cart-widget--dropdown">
+                    <div className="cart-widget--dropdown__header">
+                        <h3 className="cart-widget--dropdown__title">Shopping Cart</h3>
+                        <Link to={"/cart"} className="btn cart-widget__btn">Go to Cart</Link>
+                    </div>
+                    <div className="cart-widget--dropdown__body">
+                        {cartItems.map(card => (
+                            <div className="card compProdWidgetCard cart-widget--dropdown__item" key={card.id}>
+                                <div className='card_compare compProdWidgetCard__compare'>
+                                    <button className='card__compare-btn' onClick={() => {removeCard(card.id)}}>Remove <IoCloseCircleOutline className='icon-close'/></button>
+                                </div>
+                                <div className="compProdWidgetCard__inner" >
+                                    <div className="compProdWidgetCard__img" >
+                                        {Object.values(card.attributes?.Images).map((img, index) => ( 
+                                            <LazyLoadImage effect="opacity" src={img[0].attributes.url} key={index} alt={''} />
+                                        ))}
+                                    </div>
+                                    <div className="compProdWidgetCard__content">
+                                        <h3 className='card__title'>{card.attributes.Name}</h3>
+                                        <span className='card__price'><span>£</span>{card.attributes.Price}.00</span>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+
+        </>
+     );
+}
+ 
+export default CartWidget;
---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,80 @@
+import { Link, useLocation } from 'react-router-dom';
+import { useContext } from "react";
+import classnames from 'classnames';
+import { CompareContext } from "../../contexts/CompareContext";
+import { CartContext } from "../../contexts/CartContext";
+import { IoOptionsOutline, IoCloseCircleOutline } from 'react-icons/io5';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
+import "./ProductCard.scss";
+
+const ProductCard = (product) => {
+
+    const link = '/shop/' + product.id
+    const [compCards, setCompCards] = useContext(CompareContext);
+    const [cartItems, setCartItems] = useContext(CartContext);
+
+    // Get the current URL using the useLocation hook
+    const location = useLocation();
+
+    // Check if the current URL is the front page URL
+    const isFrontPage = location.pathname === '/';
+
+    return (
+        <div className={classnames('card grid__item', { 'card-homepage': isFrontPage })}>
+
+            <div className='card__compare'>
+                <button className={`card__compare-btn ${ compCards.find(compCard => compCard.id === product.id) !== undefined ? 'card__compare-btn--active' : '' }`}
+                    onClick={() => {
+                        if ( compCards === undefined ) {
+                            setCompCards([ product ]);
+                        } else if ( compCards.length < 3 && compCards.find(compCard => compCard.id === product.id) === undefined ) {
+                            setCompCards(prevArray => [...prevArray, product ]);
+                        } else if ( compCards.find(compCard => compCard.id === product.id) !== undefined ) {
+                            setCompCards(prevArray => prevArray.filter(compCard => compCard.id !== product.id));
+                        }
+                    }}
+                >
+                    {compCards.length === 3 && compCards.find(compCard => compCard.id === product.id) === undefined ? 
+                    <>Compare is full <IoOptionsOutline className='icon-compare'/></> : compCards.find(compCard => compCard.id === product.id) !== undefined ? 
+                    <>Compare <IoCloseCircleOutline className='icon-close'/></> : <>Compare <IoOptionsOutline className='icon-compare'/></> }
+                </button>
+            </div>
+
+            <div className="card__img" >
+                {Object.values(product.attributes.Images).map(img => ( 
+                    <Link to={link} key={img[0].id} >
+                        <LazyLoadImage effect="opacity" src={img[0].attributes.url} alt={product.attributes.Name} />
+                    </Link>
+                ))}
+            </div>
+            <h3 className="card__title"><Link to={link}>{product.attributes.Name}</Link></h3>
+            <p className='card__price'><span>£</span>{product.attributes.Price}.00</p>
+            <div className='card__submit'>
+            <button className="btn card__btn" 
+                onClick={() => {
+                    if ( cartItems === undefined ) {
+                        setCartItems([ product ]);
+                    } else {
+                        // If the product is in the cart, remove it from the cart
+                        if (cartItems.find(cartItem => cartItem.id === product.id)) {
+                            setCartItems(prevArray => prevArray.filter(cartItem => cartItem.id !== product.id));
+                        } else {
+                            // If the product is not in the cart, add it to the cart
+                            setCartItems(prevArray => [...prevArray, product]);
+                        }
+                    }
+                }}
+            >
+                { cartItems && cartItems.find(cartItem => cartItem.id === product.id) ? "Remove from cart" : "Add to cart" }
+            </button>
+                <span className="addtocart__stock">{product.attributes.Stock} <span className="inStock"></span></span>
+            </div>
+            <div className='card__readmore'>
+                <button className="btn card__btn" >Read more</button>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
