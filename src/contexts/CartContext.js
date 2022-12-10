dc0ce05a41225aa0558d5a94c0c34ef309a38e7c
Max Hargreave <max@monsun.dk>
2022-12-10 19:22:03 +0100

    cart provider, switching branches

---
+++ src/contexts/CartContext.js
@@ -0,0 +1,14 @@
+import { createContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+export const CartProvider = (props) => {
+
+    const [cartItems, setCartItems] = useState([]);
+
+    return (
+        <CartContext.Provider value={[cartItems, setCartItems]}>
+            {props.children}
+        </CartContext.Provider>
+    );
+}
