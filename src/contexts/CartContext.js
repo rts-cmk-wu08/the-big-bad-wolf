9add4b49208ecff30de52e369c849a65e98eb3a0
Max Hargreave <max@monsun.dk>
2022-12-11 01:34:17 +0100

    save cart items to local storage

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,10 +1,17 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const CartContext = createContext();
 
 export const CartProvider = (props) => {
 
-    const [cartItems, setCartItems] = useState([]);
+    // Get the initial cart items from the local storage
+    const initialCartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    const [cartItems, setCartItems] = useState(initialCartItems);
+
+    // Save the cart items to the local storage every time it changes
+    useEffect(() => {
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    }, [cartItems]);
 
     return (
         <CartContext.Provider value={[cartItems, setCartItems]}>
