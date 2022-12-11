f0d6dfaad919687175b616f4c2ab6e2542f5ebeb
Max Hargreave <max@monsun.dk>
2022-12-11 01:37:24 +0100

    compare items local storage save

--- src/contexts/CompareContext.js
+++ src/contexts/CompareContext.js
@@ -1,10 +1,17 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const CompareContext = createContext();
 
 export const CompareProvider = (props) => {
 
-    const [compCards, setCompCards] = useState([]);
+    // Get the initial compare items from the local storage
+    const initialCompCards = JSON.parse(localStorage.getItem('compCards')) || [];
+    const [compCards, setCompCards] = useState(initialCompCards);
+
+    // Save the compare items to the local storage every time it changes
+    useEffect(() => {
+        localStorage.setItem('compCards', JSON.stringify(compCards));
+    }, [compCards]);
 
     return (
         <CompareContext.Provider value={[compCards, setCompCards]}>
