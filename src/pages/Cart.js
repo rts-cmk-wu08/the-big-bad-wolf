b044de0ba0b327123b2b004c828dc3fd0312a7d9
In-Wess-Tation <skolekonto318@gmail.com>
2022-12-12 09:20:54 +0100

    cart layout done, klar til functionalitet

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,27 @@
+import "./cart.scss"
+
+
 const Cart = () => {
 
     return (
 
         <>
-        
-            <h1>Her er Cart</h1>
+        <section className="cart__icons">
+            <div className="cart icon shadow"><i className="fa-solid fa-cart-shopping cart-widget__icon"></i></div>
+            <div className="card icon shadow"><i class="fa-solid fa-credit-card"></i></div>
+            <div className="bill icon shadow"><i class="fa-solid fa-receipt"></i></div>
+            <div className="grey--line"></div>
+        </section>
+        <h1 className="page-header__title">Cart</h1>
 
+        <section className="cart__items">
+            <article className="cart__item shadow"></article>
+
+            <div className="cart__items--btn">
+                <p>Sub total <span className="orange">funtion here</span></p>
+                <button className="btn shadow">Go to payment</button>
+            </div>
+        </section>
         </>
 
      );
