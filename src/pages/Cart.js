644b3a2d4489575873227919fc8af07bf556ddb3
In-Wess-Tation <skolekonto318@gmail.com>
2022-12-12 11:23:02 +0100

    cart page fikset

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -8,8 +8,8 @@ const Cart = () => {
         <>
         <section className="cart__icons">
             <div className="cart icon shadow"><i className="fa-solid fa-cart-shopping cart-widget__icon"></i></div>
-            <div className="card icon shadow"><i class="fa-solid fa-credit-card"></i></div>
-            <div className="bill icon shadow"><i class="fa-solid fa-receipt"></i></div>
+            <div className="card icon shadow"><i className="fa-solid fa-credit-card"></i></div>
+            <div className="bill icon shadow"><i className="fa-solid fa-receipt"></i></div>
             <div className="grey--line"></div>
         </section>
         <h1 className="page-header__title">Cart</h1>
@@ -17,10 +17,12 @@ const Cart = () => {
         <section className="cart__items">
             <article className="cart__item shadow"></article>
 
-            <div className="cart__items--btn">
-                <p>Sub total <span className="orange">funtion here</span></p>
-                <button className="btn shadow">Go to payment</button>
-            </div>
+            <article className="cart__wrapper">
+                <div className="cart__items--btn">
+                    <p>Sub total <span className="orange">funtion here</span></p>
+                    <button className="btn shadow">Go to payment</button>
+                </div>
+            </article>
         </section>
         </>
 
